6249226e0b90d12fbc0705d498d1d4f329c49799
築山朋紀 <tomoki_69386@yahoo.co.jp>
2017-07-17 22:55:33 +0900

    Initial Commit

---
+++ potipoti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  potipoti
+//
+//  Created by 築山朋紀 on 2017/07/17.
+//  Copyright © 2017年 築山朋紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
